062012c673e8b0130a7cc2d464c7d514de0ad31e
mepoyraz <me.poyrazz@gmail.com>
2023-01-02 19:22:21 +0300

    Okul kaldırıldı.

--- src/components/BioCharacteristics.tsx
+++ src/components/BioCharacteristics.tsx
@@ -9,7 +9,6 @@ const BioCharacteristics = () => {
       <BioItem name="Yaş" value={`${new Date().getFullYear() - 2004}`} />
       <BioItem name="Yer" value="Türkiye / İstanbul" />
       <BioItem name="Sektör Deneyİmİ" value={`${new Date().getFullYear() - 2017} yıl`} />
-      <BioItem name="Eğİtİm" value='Beylikdüzü Borsa İstanbul Çok Programlı Anadolu Lisesi' isLast={true} />
     </div>
   )
 }
